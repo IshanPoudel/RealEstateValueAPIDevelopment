0124b5f485b936e6e8d5af3717e082485c4aa125
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-06-30 20:10:37 -0500

    Can now send status code if data does not exist.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import  Api , Resource
+from flask import Flask , request
+from flask_restful import  Api , Resource , reqparse , abort
 import mysql.connector
 
 
@@ -12,6 +12,16 @@ db = mysql.connector.connect(host="localhost" ,
     database = "real_estate")
 mycursor = db.cursor()
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name" , type = str , help = "Name of the video is required" , required= True)
+video_put_args.add_argument("views" , type = int , help = "Views of the video")
+video_put_args.add_argument("likes" , type = int , help = "Likes on the video")
+videos = {}
+
+def abort_if_video_id_doesnt_exist(video_id):
+    print("i am here")
+    if video_id not in videos:
+        abort(404 , message ="Video id not valid...")
 
 class HelloWorld(Resource):
 
@@ -31,8 +41,21 @@ class HelloWorld(Resource):
     def post(self):
         return {"data" : "Posted"}
 
-api.add_resource(HelloWorld , "/getvalue/<string:name>")
+class Video(Resource):
 
+    def get(self , video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+
+    def put(selfself , video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args #store value in video_id
+
+        return videos[video_id] , 201
+
+
+api.add_resource(HelloWorld , "/getvalue/<string:name>")
+api.add_resource(Video , "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "getvalue/Michael Brink")
 
+response = requests.put(BASE + "video/1" , {"likes" : 10 , "name" :"Ishan" ,  "views":200000} )
+
+
+#when you do request.put(Base + "getvalue/" , {}") , you can pass in an additonal list
+print(response.json())
+
+# input()
+
+#get request .
+response = requests.get(BASE + "video/6" )
 print(response.json())
